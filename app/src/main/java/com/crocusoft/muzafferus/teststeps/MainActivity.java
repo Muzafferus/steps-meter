37e12d9b525a08045ff6070b6689e4cdb85c1d66
Muzaffar Pashazade <muzaffarp@crocusoft.com>
2018-07-31 18:23:54 +0400

    Initial commit

---
+++ app/src/main/java/com/crocusoft/muzafferus/teststeps/MainActivity.java
@@ -0,0 +1,83 @@
+package com.crocusoft.muzafferus.teststeps;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
+    private TextView textStep, textKm;
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "Steps: ";
+    private static final String KM = " km";
+    private int numSteps;
+    private double numKm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get an instance of the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager != null) {
+            accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+
+        textStep = findViewById(R.id.tv_steps);
+        textKm = findViewById(R.id.tv_km);
+        Button btnStart = findViewById(R.id.btn_start);
+        Button btnStop = findViewById(R.id.btn_stop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                numSteps = 0;
+                numKm = 0;
+                sensorManager.registerListener(MainActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                sensorManager.unregisterListener(MainActivity.this);
+            }
+        });
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
+    }
+
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+        numKm = numSteps * 0.000762;
+        DecimalFormat df = new DecimalFormat("0.000");
+        String km = df.format(numKm) + KM;
+        String steps = TEXT_NUM_STEPS + numSteps;
+        textStep.setText(steps);
+        textKm.setText(km);
+    }
+}
---
+++ app/src/main/java/com/crocusoft/muzafferus/teststeps/StepListener.java
@@ -0,0 +1,8 @@
+package com.crocusoft.muzafferus.teststeps;
+
+// Will listen to step alerts
+public interface StepListener {
+
+    public void step(long timeNs);
+
+}
